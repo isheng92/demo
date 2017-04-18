5bbdb2b357c56b24c129ac14742f71bd3df3d3e8
eping92 <pb.he@skysolutions.cn>
2017-04-18 15:26:42 +0800

    ceshi

--- he/src/main/java/com/isheng/he/base/BaseEntity.java
+++ he/src/main/java/com/isheng/he/base/BaseEntity.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
  */
 public class BaseEntity implements Serializable{
 
-	private static final long serialVersionUID = 7597490832491127449L;
+	private static final long serialVersionUID = 7597490832491127447L;
 	
 	
 }
